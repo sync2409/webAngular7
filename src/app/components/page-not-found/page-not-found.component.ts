09744a9831eae9f4ff4e20f3e87a5bc50043c896
sync24september <sync24september@gmail.com>
2018-11-10 15:22:36 +0700

    22

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-page-not-found',
   template: `
     <p>
-      page-not-found works 111111!
+      page-not-found works 2222!
     </p>
   `,
   styles: []
