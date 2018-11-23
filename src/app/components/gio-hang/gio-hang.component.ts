6e044ba81a68df587f42e8bfd2aec1488ea49a6c
sync24september <sync24september@gmail.com>
2018-11-23 10:31:26 +0700

    test

--- src/app/components/gio-hang/gio-hang.component.ts
+++ src/app/components/gio-hang/gio-hang.component.ts
@@ -56,6 +56,7 @@ export class GioHangComponent implements OnInit, AfterViewInit {
   CaculatorProductTem(pID): number {
     let objformShopCart = this._formShopCart["last"].controls;
     var newPriceTem = objformShopCart["hiddPrices" + pID].value * objformShopCart["txtQuantity" + pID].value;
+
     return newPriceTem;
   }
 }
