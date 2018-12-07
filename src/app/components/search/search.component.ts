d6441e0cbb1c87df4a21877cac06b061efecbd71
sync24september <sync24september@gmail.com>
2018-12-08 00:12:51 +0700

    search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GlobalconfigService } from 'src/app/services/globalconfig.service';
 import { GlobalVariable } from 'src/app/config/global';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-search',
@@ -9,22 +10,29 @@ import { GlobalVariable } from 'src/app/config/global';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+  public BASE_URL_MEDIA = GlobalVariable.BASE_URL_MEDIA;
+  public ListProduct = [];
+  public textSearch = "";
   constructor(
     private activatedRoute: ActivatedRoute,
     private gval: GlobalconfigService,
+    private productService: ProductService
   ) { }
 
   ngOnInit() {
     this.gval.setMenuStatus(true);
     this.activatedRoute.params.subscribe(params => {
       console.log(params)
-      let textSearch = sessionStorage.getItem(GlobalVariable.TxtSearch);
+      this.textSearch = sessionStorage.getItem(GlobalVariable.TxtSearch);
       this.gval.updateBreadCrumb([
         { Name: "Trang chủ", Link: "/" },
         { Name: "Tìm kiếm", Link: "/" },
-        { Name: textSearch, Link: "/" },
+        { Name: this.textSearch, Link: "/" },
       ]);
+      this.productService.GetListProducts(0, 0, this.textSearch).subscribe((data: any) => {
+        console.log("GetListProducts search", this.textSearch, data);
+        this.ListProduct = data.ListData;
+      })
     });
   }
 
